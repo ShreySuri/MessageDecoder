4b4ae9e829a53f7317f710a514e753d44b6831c2
Shrey Suri <shreysuri5@gmail.com>
2022-05-28 22:42:36 -0500

    basic UI

--- MessageDecoder_1.py
+++ MessageDecoder_1.py
@@ -1,5 +1,10 @@
-composite = None
-while composite % 1 != 0:
-    composite = input(print("Enter the coded message. "))
 
-composite = int(composite)
+
+composite = int(input(print("Enter the coded message. ")))
+
+characters = 0
+
+while composite >= 100 ** characters:
+    characters = characters + 1
+
+
